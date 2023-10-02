2fa31cfb517933e5f1e18c693e9275694c7e51fb
vistor-souza1997 <victor-souza1997@hotmail.com>
2023-10-02 07:43:26 -0400

    add new files

--- ml.py
+++ ml.py
@@ -6,7 +6,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 # Step 1: Load the Excel file
-excel_file_path = 'data.trajeto.csv'
+excel_file_path = 'data.trajeto.xlsx'
 sheet_name = 'data.trajeto'
 
 # Load data from Excel
@@ -20,7 +20,7 @@ except Exception as e:
     exit(1)
 
 # Step 2: Check if the required columns are present
-required_columns = ['abs.angle', 'rel.angle', 'DuracaodoTrecho', 'Velocidade_km_h', 'vel.cal', 'Dif.Vel', 'cluster']
+required_columns = ['Velocidade_km_h', 'vel.cal', 'Dif.Vel', 'cluster']
 
 missing_columns = [col for col in required_columns if col not in excel_data.columns]
 if missing_columns:
@@ -29,7 +29,7 @@ if missing_columns:
 
 # Step 3: Prepare the data for neural network
 X = excel_data[required_columns[:-1]]  # Features
-y = excel_data['output_cluster']       # Target variable
+y = excel_data['cluster']       # Target variable
 
 # Encode categorical target variable (output_cluster) to numerical values
 label_encoder = LabelEncoder()
@@ -38,6 +38,7 @@ y_encoded = label_encoder.fit_transform(y)
 # Step 4: Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
 
+print(type(y_train[0]), type(y_test))
 # Step 5: Standardize the input features
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -47,16 +48,21 @@ X_test = scaler.transform(X_test)
 model = Sequential()
 model.add(Dense(64, input_dim=X_train.shape[1], activation='relu'))
 model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(32, activation='sigmoid'))
+model.add(Dense(32, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))  # Sigmoid activation for binary classification
 
 # Compile the model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer = "adam", metrics=['accuracy'])
 
 # Step 7: Train the neural network
-model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=2)
+model.fit(X_train, y_train, epochs=30, batch_size=50, verbose=3)
+print(model.summary())
 
 # Step 8: Evaluate the model on the test data
 accuracy = model.evaluate(X_test, y_test)[1]
+print(X_test)
 print(f"Accuracy: {accuracy * 100:.2f}%")
 
 # Now you have a trained neural network that can classify clusters based on the specified columns in the Excel file.
